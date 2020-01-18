d30e278851f233e8544aebc5a916a3642ad5f387
Ziyak Jehangir <53836911+ziyak97@users.noreply.github.com>
2020-01-18 17:19:47 +0530

    Add files via upload

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './search-box.styles.css'
+
+export const SearchBox = ({ placeholder, handleChange }) => {
+    return (
+        <input
+            className="search"
+            type="text"
+            placeholder={placeholder}
+            onChange={handleChange}
+        />
+    )
+
+}
